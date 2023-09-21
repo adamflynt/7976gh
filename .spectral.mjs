9c32a78e91726ef9bd0ece88ef3559e41102c77f
adam <adam.flynt@stoplight.io>
2023-09-21 13:04:32 -0500

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyMDA5MQ/spectral.js?branch_slug=main&token=964d55a1-cac7-4043-b51d-469738e89be3";
+export default { extends: ruleset };
